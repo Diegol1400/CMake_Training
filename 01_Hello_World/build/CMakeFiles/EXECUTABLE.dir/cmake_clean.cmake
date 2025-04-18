bf45d2675b59a52e7081a6bcd3549d9422c9a649
Diegol1400 <djlandinruiz@gmail.com>
2025-04-18 15:38:34 -0700

    Cmake Hello world

---
+++ 01_Hello_World/build/CMakeFiles/EXECUTABLE.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EXECUTABLE.dir/main.cpp.o"
+  "CMakeFiles/EXECUTABLE.dir/main.cpp.o.d"
+  "EXECUTABLE"
+  "EXECUTABLE.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EXECUTABLE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
